db4e26be4c4710430e648e56042fdd4be3282e1e
biotrump <support@biotrump.com>
2014-07-23 10:02:52 +0800

    compiling in VS2010

--- runtime/samples/C/sample.c
+++ runtime/samples/C/sample.c
@@ -19,8 +19,13 @@
 
 #include <stdio.h>
 
-#include <cv.h>
-#include <highgui.h>
+//#include <cv.h>
+//#include <highgui.h>
+
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#define _ROTATION_INVARIANT_DETECTION_
 
 /*
 	
