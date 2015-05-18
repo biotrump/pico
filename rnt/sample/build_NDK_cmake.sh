753a9a091e9031f4fab388245e045bd7a824d6a7
thomas tsai <thomas@biotrump.com>
2015-05-18 13:46:13 +0800

    1. NDK multi platforms build: libpicort.a out/cv/pico/libs arm64-v8a  armeabi  armeabi-v7a  mips  mips64  x86  x86_64
    2. Stop build if error happens!

--- rnt/sample/build_NDK_cmake.sh
+++ rnt/sample/build_NDK_cmake.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
-	echo "$0 [arm|x86]"
-	exit
-fi
 #export CMAKE_BUILD_TYPE "Debug"
 export CMAKE_BUILD_TYPE="Release"
 
-# Make sure you have NDK_ROOT defined in .bashrc or .bash_profile
-# Modify INSTALL_DIR to suit your situation
-#Lollipop	5.0 - 5.1	API level 21, 22
-#KitKat	4.4 - 4.4.4	API level 19
-#Jelly Bean	4.3.x	API level 18
-#Jelly Bean	4.2.x	API level 17
-#Jelly Bean	4.1.x	API level 16
-#Ice Cream Sandwich	4.0.3 - 4.0.4	API level 15, NDK 8
-#Ice Cream Sandwich	4.0.1 - 4.0.2	API level 14, NDK 7
-#Honeycomb	3.2.x	API level 13
-#Honeycomb	3.1	API level 12, NDK 6
-#Honeycomb	3.0	API level 11
-#Gingerbread	2.3.3 - 2.3.7	API level 10
-#Gingerbread	2.3 - 2.3.2	API level 9, NDK 5
-#Froyo	2.2.x	API level 8, NDK 4
+
 
 if [ -z "${NDK_ROOT}"  ]; then
 	export NDK_ROOT=${HOME}/NDK/android-ndk-r10d
-#	export NDK_ROOT=${HOME}/NDK/android-ndk-r9
+	#export NDK_ROOT=${HOME}/NDK/android-ndk-r9
 fi
 export ANDROID_NDK=${NDK_ROOT}
+
+while [ $# -ge 1 ]; do
+	case $1 in
+	-ABI|-abi)
+		echo "\$1=-abi"
+		shift
+		APP_ABI=$1
+		shift
+		;;
+	-clean|-c|-C) #
+		echo "\$1=-c,-C,-clean"
+		clean_build=1
+		shift
+		;;
+	-l|-L)
+		echo "\$1=-l,-L"
+		local_build=1
+		;;
+	--help|-h|-H)
+		# The main case statement will give a usage message.
+		echo "$0 -c|-clean -abi=[armeabi, armeabi-v7a, armv8-64,mips,mips64el, x86,x86_64]"
+		exit 1
+		break
+		;;
+	-*)
+		echo "$0: unrecognized option $1" >&2
+		exit 1
+		;;
+	*)
+		break
+		;;
+	esac
+done
+echo APP_ABI=$APP_ABI
+export APP_ABI
 
 if [[ ${NDK_ROOT} =~ .*"-r9".* ]]
 then
@@ -38,70 +55,104 @@ ANDROID_APIVER=android-14
 #gfortran is in r9d V4.8.0
 TOOL_VER="4.8.0"
 else
-#android 4.0.1 ICS and above
-ANDROID_APIVER=android-14
+#r10d : android 4.0.1 ICS and above
+if [ "$APP_ABI" = "arm64-v8a" -o \
+	"$APP_ABI" = "x86_64" -o \
+	"$APP_ABI" = "mips64" ]; then
+	ANDROID_APIVER=android-21
+else
+	ANDROID_APIVER=android-14
+fi
 TOOL_VER="4.9"
 fi
-
-case $(uname -s) in
-  Darwin)
-    CONFBUILD=i386-apple-darwin`uname -r`
-    HOSTPLAT=darwin-x86
-    CORE_COUNT=`sysctl -n hw.ncpu`
-  ;;
-  Linux)
-    CONFBUILD=x86-unknown-linux
-    HOSTPLAT=linux-`uname -m`
-    CORE_COUNT=`grep processor /proc/cpuinfo | wc -l`
-  ;;
-CYGWIN*)
-	CORE_COUNT=`grep processor /proc/cpuinfo | wc -l`
-	;;
-  *) echo $0: Unknown platform; exit
-esac
-
-#NDK cross compile toolchains
+#echo ANDROID_APIVER=$ANDROID_APIVER
+#read
 #default is arm
-export ARCHI=$1
-echo ARCHI=$ARCHI
-case $ARCHI in
-  arm)
+#export PATH="$NDK_ROOT/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin/:\
+#$NDK_ROOT/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/${TARGPLAT}/bin/:$PATH"
+case $APP_ABI in
+  armeabi)
     TARGPLAT=arm-linux-androideabi
-    CONFTARG=arm-eabi
-	echo "Using: $NDK_ROOT/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin"
-	#export PATH="$NDK_ROOT/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin/:\
-	#$NDK_ROOT/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/${TARGPLAT}/bin/:$PATH"
-	export PATH="${NDK_ROOT}/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin/:$PATH"
-  ;;
-  x86)
-    TARGPLAT=i686-linux-android
-    CONFTARG=x86
-	echo "Using: $NDK_ROOT/toolchains/x86-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin"
-	export PATH="${NDK_ROOT}/toolchains/x86-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin/:$PATH"
-#specify assembler for x86 SSE3, but ffts's sse.s needs 64bit x86.
-#intel atom z2xxx and the old atoms are 32bit, so 64bit x86 in android can't work in
-#most atom devices.
-#http://forum.cvapp.org/viewtopic.php?f=13&t=423&sid=4c47343b1de899f9e1b0d157d04d0af1
-#	export  CCAS="${TARGPLAT}-as"
-#	export  CCASFLAGS="--64 -march=i686+sse3"
-#	export  CCASFLAGS="--64"
+    TOOLCHAINS=arm-linux-androideabi
+    ARCH=arm
 
+	export BLIS_LIB_NAME=${BLIS_LIB_NAME:-blis-NDK-arm}
+	FFTS_LIB_NAME=ffts
+	#libpico-NDK-arm.a
+	PICORT_LIB_NAME=picort
+	#enable VFP only
+  ;;
+  armeabi-v7a)
+    TARGPLAT=arm-linux-androideabi
+    TOOLCHAINS=arm-linux-androideabi
+    ARCH=arm
+	#enable NEON
+	export BLIS_LIB_NAME=${BLIS_LIB_NAME:-blis-NDK-arm}
+	FFTS_LIB_NAME=ffts
+	PICORT_LIB_NAME=picort
+  ;;
+  arm64-v8a)
+    TARGPLAT=aarch64-linux-android
+    TOOLCHAINS=aarch64-linux-android
+    ARCH=arm64
+
+	export BLIS_LIB_NAME=${BLIS_LIB_NAME:-blis-NDK-arm}
+	FFTS_LIB_NAME=ffts
+	PICORT_LIB_NAME=picort
+  ;;
+  x86)#atom-32
+    TARGPLAT=i686-linux-android
+    TOOLCHAINS=x86
+    ARCH=x86
+	#specify assembler for x86 SSE3, but ffts's sse.s needs 64bit x86.
+	#intel atom z2xxx and the old atoms are 32bit
+	#http://forum.cvapp.org/viewtopic.php?f=13&t=423&sid=4c47343b1de899f9e1b0d157d04d0af1
+	export  CCAS="${TARGPLAT}-as"
+	export  CCASFLAGS="--32 -march=i686+sse3"
+
+	export BLIS_LIB_NAME=${BLIS_LIB_NAME:-blis-NDK-x86}
+	PICORT_LIB_NAME=picort
+
+	echo "$APP_ABI is not supported in dsplib yet!!!"
+  ;;
+  x86_64)
+    TARGPLAT=x86_64-linux-android
+    TOOLCHAINS=x86_64
+    ARCH=x86_64
+    #specify assembler for x86 SSE3, but ffts's sse.s needs 64bit x86.
+	#atom-64 or x86-64 devices only.
+	#http://forum.cvapp.org/viewtopic.php?f=13&t=423&sid=4c47343b1de899f9e1b0d157d04d0af1
+	export  CCAS="${TARGPLAT}-as"
+#	export  CCASFLAGS="--64 -march=i686+sse3"
+	export  CCASFLAGS="--64"
+
+	export BLIS_LIB_NAME=${BLIS_LIB_NAME:-blis-NDK-x86}
+	PICORT_LIB_NAME=picort
   ;;
   mips)
-  ## probably wrong
-    TARGPLAT=mipsel-linux-android
-    CONFTARG=mips
+	## probably wrong
+	TARGPLAT=mipsel-linux-android
+	TOOLCHAINS=mipsel-linux-android
+	ARCH=mips
+	echo "$APP_ABI is not supported in dsplib yet!!!"
+  ;;
+  mips64)
+	## probably wrong
+	TARGPLAT=mips64el-linux-android
+	TOOLCHAINS=mips64el-linux-android
+	ARCH=mips64
+	echo "$APP_ABI is not supported in FFTS yet!!!"
   ;;
   *) echo $0: Unknown target; exit
 esac
-#: ${NDK_ROOT:?}
-echo $PATH
 
-export SYS_ROOT="${NDK_ROOT}/platforms/${ANDROID_APIVER}/arch-${ARCHI}/"
+echo "Using: $NDK_ROOT/toolchains/${TOOLCHAINS}-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin"
+export PATH="${NDK_ROOT}/toolchains/${TOOLCHAINS}-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin/:$PATH"
+
+export SYS_ROOT="${NDK_ROOT}/platforms/${ANDROID_APIVER}/arch-${ARCH}/"
 export CC="${TARGPLAT}-gcc --sysroot=$SYS_ROOT"
 export LD="${TARGPLAT}-ld"
 export AR="${TARGPLAT}-ar"
-export ARCH=${AR}
 export RANLIB="${TARGPLAT}-ranlib"
 export STRIP="${TARGPLAT}-strip"
 #export CFLAGS="-Os -fPIE"
@@ -111,8 +162,8 @@ export FORTRAN="${TARGPLAT}-gfortran --sysroot=$SYS_ROOT"
 
 #!!! quite importnat for cmake to define the NDK's fortran compiler.!!!
 #Don't let cmake decide it.
-#export FC=/home/thomas/aosp/NDK/android-ndk-r9/toolchains/arm-linux-androideabi-4.8.0/prebuilt/linux-x86/bin/arm-linux-androideabi-gfortran
 export FC=${FORTRAN}
+export AM_ANDROID_EXTRA="-llog -fPIE -pie"
 
 #include path :
 #platforms/android-21/arch-arm/usr/include/
@@ -130,75 +181,149 @@ else
 fi
 export LAPACKE_SRC=${LAPACKE_SRC:-${LAPACK_SRC}/LAPACKE}
 export CBLAS_SRC=${CBLAS_SRC:-${LAPACK_SRC}/CBLAS}
-export PICORT_HOME=${PICORT_HOME:-`pwd`}
+export PICORT_DIR=${PICORT_DIR:-`pwd`}
 
-case $ARCHI in
-	arm)
-		#armv7a
-		#$BLIS_OUT/lib/libblis-NDK-arm.a
-		export BLIS_LIB_NAME=${BLIS_LIB_NAME:-blis-NDK-arm}
-		#export FFTS_LIB_DIR=${FFTS_OUT}/lib
-		#libffts-NDK-arm.a
-		FFTS_LIB_NAME=ffts-NDK-${TARGET_ARCH}
-		#export LAPACK_BUILD=${LAPACK_BUILD:-${LAPACK_SRC}/build_NDK_arm}
-		;;
-	x86)
-		#x86
-		#$BLIS_OUT/lib/libblis-NDK-x86.a
-		export BLIS_LIB_NAME=${BLIS_LIB_NAME:-blis-NDK-x86}
-		#export FFTS_LIB_DIR=${FFTS_OUT}/lib
-		#libffts-NDK-x86.a
-		FFTS_LIB_NAME=ffts-NDK-x86
-		#export LAPACK_BUILD=${LAPACK_BUILD:-${LAPACK_SRC}/build_NDK_x86}
-	;;
-	mips)
-	;;
-	*) echo $0: Unknown target; exit
-esac
-if [ ! -d ${PICO_OUT} ]; then
-	mkdir -p ${PICO_OUT}
-else
-	rm -rf ${PICO_OUT}/*
+if [ -z "$PICO_OUT" ]; then
+	export PICO_OUT=${PICO_OUT:-$PICO_DIR/build}
+	export local_build=1
 fi
-pushd ${PICO_OUT}
 
-case $ARCHI in
-  arm)
-	cmake -DCMAKE_TOOLCHAIN_FILE=${PICORT_HOME}/android.toolchain.cmake \
+#check if it needs a clean build?
+if [ -d "$PICO_OUT/$APP_ABI" ]; then
+	if [ -n "$clean_build" ]; then
+		rm -rf $PICO_OUT/$APP_ABI/*
+	fi
+else
+	mkdir -p $PICO_OUT/$APP_ABI
+fi
+
+pushd ${PICO_OUT}/$APP_ABI
+
+case $APP_ABI in
+	armeabi)
+	cmake -DCMAKE_TOOLCHAIN_FILE=${PICORT_DIR}/android.toolchain.cmake \
 	-DANDROID_NATIVE_API_LEVEL=${ANDROID_APIVER} \
-	-DANDROID_NDK=${NDK_ROOT} -DANDROID_TOOLCHAIN_NAME=${TARGPLAT}-${TOOL_VER} \
-	-DCMAKE_BUILD_TYPE=Release -DANDROID_ABI="armeabi-v7a with VFPV3" \
+	-DANDROID_NDK=${ANDROID_NDK} -DANDROID_TOOLCHAIN_NAME=${TOOLCHAINS}-${TOOL_VER} \
+	-DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=$APP_ABI \
 	-DFFTS_DIR:FILEPATH=${FFTS_DIR} -DFFTS_LIB_NAME=${FFTS_LIB_NAME} \
 	-DFFTS_OUT:FILEPATH=${FFTS_OUT} \
 	-DLAPACK_SRC:FILEPATH=${LAPACK_SRC} -DLAPACKE_SRC:FILEPATH=${LAPACKE_SRC} \
 	-DLAPACK_OUT:FILEPATH=${LAPACK_OUT} \
 	-DUSE_BLIS=1 -DBLIS_DIR:FILEPATH=${BLIS_DIR} -DBLISLIB_DIR:FILEPATH=${BLISLIB_DIR} \
 	-DBLIS_LIB_NAME=${BLIS_LIB_NAME} \
-	-DTARGET_ARCH=${TARGET_ARCH} \
-	-DCBLAS_SRC:FILEPATH=${CBLAS_SRC} ${PICORT_HOME}
-
+	-DAPP_ABI=${APP_ABI} \
+	-DCBLAS_SRC:FILEPATH=${CBLAS_SRC} ${PICORT_DIR}
+	;;
+	armeabi-v7a)
+	cmake -DCMAKE_TOOLCHAIN_FILE=${PICORT_DIR}/android.toolchain.cmake \
+	-DANDROID_NATIVE_API_LEVEL=${ANDROID_APIVER} \
+	-DANDROID_NDK=${ANDROID_NDK} -DANDROID_TOOLCHAIN_NAME=${TOOLCHAINS}-${TOOL_VER} \
+	-DCMAKE_BUILD_TYPE=Release -DANDROID_ABI="armeabi-v7a with NEON" \
+	-DFFTS_DIR:FILEPATH=${FFTS_DIR} -DFFTS_LIB_NAME=${FFTS_LIB_NAME} \
+	-DFFTS_OUT:FILEPATH=${FFTS_OUT} \
+	-DLAPACK_SRC:FILEPATH=${LAPACK_SRC} -DLAPACKE_SRC:FILEPATH=${LAPACKE_SRC} \
+	-DLAPACK_OUT:FILEPATH=${LAPACK_OUT} \
+	-DUSE_BLIS=1 -DBLIS_DIR:FILEPATH=${BLIS_DIR} -DBLISLIB_DIR:FILEPATH=${BLISLIB_DIR} \
+	-DBLIS_LIB_NAME=${BLIS_LIB_NAME} \
+	-DAPP_ABI=${APP_ABI} \
+	-DCBLAS_SRC:FILEPATH=${CBLAS_SRC} ${PICORT_DIR}
+	;;
+	arm64-v8a)
+	cmake -DCMAKE_TOOLCHAIN_FILE=${PICORT_DIR}/android.toolchain.cmake \
+	-DANDROID_NATIVE_API_LEVEL=${ANDROID_APIVER} \
+	-DANDROID_NDK=${ANDROID_NDK} -DANDROID_TOOLCHAIN_NAME=${TOOLCHAINS}-${TOOL_VER} \
+	-DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=$APP_ABI \
+	-DFFTS_DIR:FILEPATH=${FFTS_DIR} -DFFTS_LIB_NAME=${FFTS_LIB_NAME} \
+	-DFFTS_OUT:FILEPATH=${FFTS_OUT} \
+	-DLAPACK_SRC:FILEPATH=${LAPACK_SRC} -DLAPACKE_SRC:FILEPATH=${LAPACKE_SRC} \
+	-DLAPACK_OUT:FILEPATH=${LAPACK_OUT} \
+	-DUSE_BLIS=1 -DBLIS_DIR:FILEPATH=${BLIS_DIR} -DBLISLIB_DIR:FILEPATH=${BLISLIB_DIR} \
+	-DBLIS_LIB_NAME=${BLIS_LIB_NAME} \
+	-DAPP_ABI=${APP_ABI} \
+	-DCBLAS_SRC:FILEPATH=${CBLAS_SRC} ${PICORT_DIR}
 	;;
   x86)
-	cmake -DCMAKE_TOOLCHAIN_FILE=${PICORT_HOME}/android.toolchain.cmake \
+	cmake -DCMAKE_TOOLCHAIN_FILE=${PICORT_DIR}/android.toolchain.cmake \
 	-DANDROID_NATIVE_API_LEVEL=${ANDROID_APIVER} \
-	-DANDROID_NDK=${NDK_ROOT} -DANDROID_TOOLCHAIN_NAME=x86-${TOOL_VER} \
-	-DCMAKE_BUILD_TYPE=Release -DANDROID_ABI="x86" \
+	-DANDROID_NDK=${NDK_ROOT} -DANDROID_TOOLCHAIN_NAME=${TOOLCHAINS}-${TOOL_VER} \
+	-DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=$APP_ABI \
 	-DFFTS_DIR:FILEPATH=${FFTS_DIR} -DFFTS_LIB_NAME=${FFTS_LIB_NAME} \
 	-DFFTS_OUT:FILEPATH=${FFTS_OUT} \
 	-DLAPACK_SRC:FILEPATH=${LAPACK_SRC} -DLAPACKE_SRC:FILEPATH=${LAPACKE_SRC} \
 	-DLAPACK_OUT:FILEPATH=${LAPACK_OUT} \
 	-DUSE_BLIS=1 -DBLIS_DIR:FILEPATH=${BLIS_DIR} -DBLISLIB_DIR:FILEPATH=${BLISLIB_DIR} \
 	-DBLIS_LIB_NAME=${BLIS_LIB_NAME} \
-	-DTARGET_ARCH=${TARGET_ARCH} \
-	-DCBLAS_SRC:FILEPATH=${CBLAS_SRC} ${PICORT_HOME}
-
+	-DAPP_ABI=${APP_ABI} \
+	-DCBLAS_SRC:FILEPATH=${CBLAS_SRC} ${PICORT_DIR}
+  ;;
+  x86_64)
+	cmake -DCMAKE_TOOLCHAIN_FILE=${PICORT_DIR}/android.toolchain.cmake \
+	-DANDROID_NATIVE_API_LEVEL=${ANDROID_APIVER} \
+	-DANDROID_NDK=${NDK_ROOT} -DANDROID_TOOLCHAIN_NAME=${TOOLCHAINS}-${TOOL_VER} \
+	-DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=$APP_ABI \
+	-DFFTS_DIR:FILEPATH=${FFTS_DIR} -DFFTS_LIB_NAME=${FFTS_LIB_NAME} \
+	-DFFTS_OUT:FILEPATH=${FFTS_OUT} \
+	-DLAPACK_SRC:FILEPATH=${LAPACK_SRC} -DLAPACKE_SRC:FILEPATH=${LAPACKE_SRC} \
+	-DLAPACK_OUT:FILEPATH=${LAPACK_OUT} \
+	-DUSE_BLIS=1 -DBLIS_DIR:FILEPATH=${BLIS_DIR} -DBLISLIB_DIR:FILEPATH=${BLISLIB_DIR} \
+	-DBLIS_LIB_NAME=${BLIS_LIB_NAME} \
+	-DAPP_ABI=${APP_ABI} \
+	-DCBLAS_SRC:FILEPATH=${CBLAS_SRC} ${PICORT_DIR}
   ;;
   mips)
+	cmake -DCMAKE_TOOLCHAIN_FILE=${PICORT_DIR}/android.toolchain.cmake \
+	-DANDROID_NATIVE_API_LEVEL=${ANDROID_APIVER} \
+	-DANDROID_NDK=${NDK_ROOT} -DANDROID_TOOLCHAIN_NAME=${TOOLCHAINS}-${TOOL_VER} \
+	-DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=$APP_ABI \
+	-DFFTS_DIR:FILEPATH=${FFTS_DIR} -DFFTS_LIB_NAME=${FFTS_LIB_NAME} \
+	-DFFTS_OUT:FILEPATH=${FFTS_OUT} \
+	-DLAPACK_SRC:FILEPATH=${LAPACK_SRC} -DLAPACKE_SRC:FILEPATH=${LAPACKE_SRC} \
+	-DLAPACK_OUT:FILEPATH=${LAPACK_OUT} \
+	-DUSE_BLIS=1 -DBLIS_DIR:FILEPATH=${BLIS_DIR} -DBLISLIB_DIR:FILEPATH=${BLISLIB_DIR} \
+	-DBLIS_LIB_NAME=${BLIS_LIB_NAME} \
+	-DAPP_ABI=${APP_ABI} \
+	-DCBLAS_SRC:FILEPATH=${CBLAS_SRC} ${PICORT_DIR}
+  ;;
+  mips64)
+	cmake -DCMAKE_TOOLCHAIN_FILE=${PICORT_DIR}/android.toolchain.cmake \
+	-DANDROID_NATIVE_API_LEVEL=${ANDROID_APIVER} \
+	-DANDROID_NDK=${NDK_ROOT} -DANDROID_TOOLCHAIN_NAME=${TOOLCHAINS}-${TOOL_VER} \
+	-DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=$APP_ABI \
+	-DFFTS_DIR:FILEPATH=${FFTS_DIR} -DFFTS_LIB_NAME=${FFTS_LIB_NAME} \
+	-DFFTS_OUT:FILEPATH=${FFTS_OUT} \
+	-DLAPACK_SRC:FILEPATH=${LAPACK_SRC} -DLAPACKE_SRC:FILEPATH=${LAPACKE_SRC} \
+	-DLAPACK_OUT:FILEPATH=${LAPACK_OUT} \
+	-DUSE_BLIS=1 -DBLIS_DIR:FILEPATH=${BLIS_DIR} -DBLISLIB_DIR:FILEPATH=${BLISLIB_DIR} \
+	-DBLIS_LIB_NAME=${BLIS_LIB_NAME} \
+	-DAPP_ABI=${APP_ABI} \
+	-DCBLAS_SRC:FILEPATH=${CBLAS_SRC} ${PICORT_DIR}
   ;;
   *) echo $0: Unknown target; exit
 esac
 
+ret=$?
+echo "ret=$ret"
+if [ "$ret" != '0' ]; then
+echo "$0 cmake error!!!!"
+exit -1
+fi
+
 make -j${CORE_COUNT}
+
+ret=$?
+echo "ret=$ret"
+if [ "$ret" != '0' ]; then
+echo "$0 make error!!!!"
+exit -1
+fi
 
 popd
 
+pushd ${PICO_OUT}
+mkdir -p libs/$APP_ABI
+#cp ${PICORT_DIR}/Android.mk ${PICO_OUT}/libs
+rm -rf libs/$APP_ABI/*
+ln -s ${PICO_OUT}/$APP_ABI/lib/libpicort.a libs/$APP_ABI/libpicort.a
+popd
+
+exit 0
